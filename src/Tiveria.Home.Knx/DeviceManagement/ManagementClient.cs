a88b7f8ccc2823523191945c494ac49256e22ea6
Michael Geissler <7744975+toolsfactory@users.noreply.github.com>
2022-01-25 08:26:42 +0100

    * Initial Management Client

---
+++ src/Tiveria.Home.Knx/DeviceManagement/ManagementClient.cs
@@ -0,0 +1,66 @@
+﻿/*
+    Tiveria.Home.Knx - a .Net Core base KNX library
+    Copyright (c) 2018 M. Geissler
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation; either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+    Linking this library statically or dynamically with other modules is
+    making a combined work based on this library. Thus, the terms and
+    conditions of the GNU General Public License cover the whole
+    combination.
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Tiveria.Home.Knx.Adresses;
+using Tiveria.Home.Knx.Cemi;
+
+namespace Tiveria.Home.Knx.DeviceManagement
+{
+    public class ManagementClient : IManagementClient
+    {
+        private IKnxClient _client;
+
+        public ManagementClient(IKnxClient client)
+        {
+            _client = client;
+        }
+
+
+        public async Task ConnectAsync()
+        {
+            var tpdu = new Tpci(PacketType.Control, SequenceType.UnNumbered, 0, ControlType.Connect);
+            var ctrl1 = new ControlField1(MessageCode.LDATA_REQ);
+            var ctrl2 = new ControlField2();
+            var cemi = new CemiLData(Cemi.MessageCode.LDATA_REQ, new List<AdditionalInformationField>(), new IndividualAddress(0, 0, 0), GroupAddress.Parse("4/0/0"), ctrl1, ctrl2, tpdu);
+            await _client.SendCemiAsync(cemi);
+        }
+
+        public async Task DisconnectAsync()
+        {
+            var tpdu = new Tpci(Cemi.PacketType.Control, Cemi.SequenceType.UnNumbered, 0, ControlType.Disconnect);
+            var ctrl1 = new ControlField1(MessageCode.LDATA_REQ);
+            var ctrl2 = new ControlField2();
+            var cemi = new Cemi.CemiLData(Cemi.MessageCode.LDATA_REQ, new List<AdditionalInformationField>(), new IndividualAddress(0, 0, 0), GroupAddress.Parse("4/0/0"), ctrl1, ctrl2, tpdu);
+            await _client.SendCemiAsync(cemi);
+        }
+
+        public Task<T> ReadPropertyAsync<T>(byte objIdx, byte propId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
